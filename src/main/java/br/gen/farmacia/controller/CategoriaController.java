5654d7ffa41703d3d06cfed421dc11c51cf1e20c
Rodrigo Baba <rodrigonuciatelli@gmail.com>
2022-01-25 18:59:46 -0300

    @RodrigoBaba [Criado metodo getById]

--- src/main/java/br/gen/farmacia/controller/CategoriaController.java
+++ src/main/java/br/gen/farmacia/controller/CategoriaController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -34,5 +35,11 @@ public class CategoriaController {
 		}
 	}
 	
-	
+	@GetMapping("/id/{id_categoria}")
+	public ResponseEntity<Categoria> getById(@PathVariable(value = "id_categoria") Long id){
+		return repository.findById(id).map(resp -> ResponseEntity.status(200).body(resp))
+				.orElseGet(() -> {
+					throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Id não encontrado!");
+				});
+	}
 }
