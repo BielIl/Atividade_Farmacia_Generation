2fdc859597f5b313fe09fbd896d9483c56063358
Gabriel Luis Nolasco <gl450681@gmail.com>
2022-01-25 19:43:38 -0300

    @BielIl [add delete]
    adicionando o delete a categoria controller

--- src/main/java/br/gen/farmacia/controller/CategoriaController.java
+++ src/main/java/br/gen/farmacia/controller/CategoriaController.java
@@ -1,11 +1,13 @@
 package br.gen.farmacia.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -73,6 +75,23 @@ public class CategoriaController {
 		
 	}
 	
-	
+	//DELETE
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<?> delete(@PathVariable Long id){
+		
+		Optional<Categoria> optional = repository.findById(id);
+		
+			if (optional.isPresent()) {
+				
+				repository.deleteById(id);
+				return ResponseEntity.status(200).build();
+				
+			} else {
+				
+				throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Id não encontrado!");
+				
+			}
+			
+	}
 	
 }
