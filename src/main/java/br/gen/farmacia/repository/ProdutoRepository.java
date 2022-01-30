f770ce52689da0657f92e1b5a252fff267e78137
Lucas <watchlucas274@gmail.com>
2022-01-30 13:24:02 -0300

    [Controller e Repository de Produto feitas]

--- src/main/java/br/gen/farmacia/repository/ProdutoRepository.java
+++ src/main/java/br/gen/farmacia/repository/ProdutoRepository.java
@@ -1,8 +1,15 @@
 package br.gen.farmacia.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import br.gen.farmacia.model.ProdutoModel;
 
 public interface ProdutoRepository extends JpaRepository<ProdutoModel, Long> {
+	
+public List<ProdutoModel> findAllByNomeContainingIgnoreCase(String nome);
+
+
 	
 }
