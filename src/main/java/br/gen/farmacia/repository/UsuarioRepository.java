0188f7b43623122ef33ff766a71615a59a7e82f6
Lucas <watchlucas274@gmail.com>
2022-01-31 11:53:57 -0300

    @Lucxsss[Finalizando interface repository]

--- src/main/java/br/gen/farmacia/repository/UsuarioRepository.java
+++ src/main/java/br/gen/farmacia/repository/UsuarioRepository.java
@@ -1,5 +1,13 @@
 package br.gen.farmacia.repository;
 
-public interface UsuarioRepository {
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.gen.farmacia.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	public Optional<Usuario> findByEmail(String email);
 }
