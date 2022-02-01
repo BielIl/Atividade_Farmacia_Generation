8626e772907d449208340b5a6e52a782e1450643
isabelalberti <isabelalberti84@gmail.com>
2022-02-01 11:32:01 -0300

    @isabelalberti [corrigindo conflitos]

--- src/main/java/br/gen/farmacia/model/UsuarioLogin.java
+++ src/main/java/br/gen/farmacia/model/UsuarioLogin.java
@@ -1,54 +1,16 @@
 package br.gen.farmacia.model;
 
 public class UsuarioLogin {
-
-<<<<<<< HEAD
-		private String name;
-			
-		private String email;
-		
-		private String password;
-		
-		private String token;
-=======
 	private String name;
->>>>>>> f5b76fbca58d5567010db828d3d9f8a62576226f
 
 	private String email;
 
 	private String password;
 
-<<<<<<< HEAD
-		public String getEmail() {
-			return email;
-		}
-
-		public void setEmail(String email) {
-			this.email = email;
-		}
-
-		public String getPassword() {
-			return password;
-		}
-
-		public void setPassword(String password) {
-			this.password = password;
-		}
-
-		public String getToken() {
-			return token;
-		}
-
-		public void setToken(String token) {
-			this.token = token;
-		}
-
-=======
 	private String token;
 
 	public String getName() {
 		return name;
->>>>>>> f5b76fbca58d5567010db828d3d9f8a62576226f
 	}
 
 	public void setName(String name) {
@@ -79,4 +41,4 @@ public class UsuarioLogin {
 		this.token = token;
 	}
 
-}
+}	
